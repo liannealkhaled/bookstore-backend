19715659399bf223141bdeb8901e7d027ac02284
lianne <liannealq@hotmail.com>
2023-11-25 02:38:04 +0300

    add genre

--- Api/Genre/genre.routes.js
+++ Api/Genre/genre.routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { getAllGenre, addGenre } = require("./genre.controllers");
+const passport = require("passport");
 
 const genrerouter = express.Router();
 
 genrerouter.get("/allgenre", getAllGenre);
-genrerouter.post("/addGenre", addGenre);
+genrerouter.post(
+  "/addGenre",
+  passport.authenticate("jwt", { session: false }),
+  addGenre
+);
 
 module.exports = genrerouter;
